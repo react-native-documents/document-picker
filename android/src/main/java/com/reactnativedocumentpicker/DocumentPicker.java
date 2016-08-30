4d436e7374005dc547a0680c51c5bd0ce95387f3
npomfret <nick-github@snowmonkey.co.uk>
2016-08-30 21:34:35 +0100

    added empty onNewIntent method which is required apparently

--- android/src/main/java/com/reactnativedocumentpicker/DocumentPicker.java
+++ android/src/main/java/com/reactnativedocumentpicker/DocumentPicker.java
@@ -111,4 +111,7 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
 
         return map;
     }
+
+    // Required for RN 0.30+ modules than implement ActivityEventListener
+    public void onNewIntent(Intent intent) { }
 }
