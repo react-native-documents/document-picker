5920b1fb43ce866f0d7b952987c9a12b0d7b38c3
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2017-12-19 22:49:34 +0100

    Add support for `web` platform
    This makes it compatible with
    [react-native-web](https://github.com/necolas/react-native-web)

---
+++ web/index.js
@@ -0,0 +1,42 @@
+const E_DOCUMENT_PICKER_CANCELED = "DOCUMENT_PICKER_CANCELED"
+
+
+function mapFiles(file)
+{
+  file.uri = URL.createObjectURL(file)
+
+  return file
+}
+
+
+function pick({multiple, type})
+{
+  const input = document.createElement('input')
+
+  const promise = new Promise(function(resolve, reject)
+  {
+    function onchange()
+    {
+      input.removeEventListener('change', onchange)
+
+      const {files} = this
+      // TODO Not working, there's no easy way to detect `cancel` button
+      if(!files.length) return reject(E_DOCUMENT_PICKER_CANCELED)
+
+      resolve(Array.prototype.map.call(files, mapFiles))
+    }
+
+    input.addEventListener('change', onchange)
+  })
+
+  input.accept = type.join(',')
+  input.multiple = multiple
+  input.type = 'file'
+
+  input.click()
+
+  return promise
+}
+
+
+exports.pick = pick
