2db3c374fac937baf713f69e8d29e0b2c0c696b3
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2017-12-20 09:37:30 +0100

    Adjusted code style to other Javascript files in the project

--- web/index.js
+++ web/index.js
@@ -1,42 +1,44 @@
-const E_DOCUMENT_PICKER_CANCELED = "DOCUMENT_PICKER_CANCELED"
+const E_DOCUMENT_PICKER_CANCELED = "DOCUMENT_PICKER_CANCELED";
 
 
 function mapFiles(file)
 {
-  file.uri = URL.createObjectURL(file)
+  file.uri = URL.createObjectURL(file);
 
-  return file
+  return file;
 }
 
 
 function pick({multiple, type})
 {
-  const input = document.createElement('input')
+  const input = document.createElement('input');
 
   const promise = new Promise(function(resolve, reject)
   {
     function onchange()
     {
-      input.removeEventListener('change', onchange)
+      input.removeEventListener('change', onchange);
 
-      const {files} = this
+      const {files} = this;
       // TODO Not working, there's no easy way to detect `cancel` button
-      if(!files.length) return reject(E_DOCUMENT_PICKER_CANCELED)
+      if ( !files.length ) {
+        return reject(E_DOCUMENT_PICKER_CANCELED);
+      }
 
-      resolve(Array.prototype.map.call(files, mapFiles))
+      resolve(Array.prototype.map.call(files, mapFiles));
     }
 
-    input.addEventListener('change', onchange)
-  })
+    input.addEventListener('change', onchange);
+  });
 
-  input.accept = type.join(',')
-  input.multiple = multiple
-  input.type = 'file'
+  input.accept = type.join(',');
+  input.multiple = multiple;
+  input.type = 'file';
 
-  input.click()
+  input.click();
 
-  return promise
+  return promise;
 }
 
 
-exports.pick = pick
+exports.pick = pick;
