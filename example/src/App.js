b31a7ebf709849d26cc9346c4f8a6ba080f5d43a
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-04-17 22:51:10 +0200

    Change example project to show list of picked files

--- example/src/App.js
+++ example/src/App.js
@@ -5,57 +5,55 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {Button, ListView, Text, View} from 'react-native';
 
 import DocumentPicker from 'react-native-document-picker'
 
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+function rowHasChanged(r1, r2)
+{
+  return r1 !== r2
+}
+const ds = new ListView.DataSource({rowHasChanged})
+
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
+  constructor() {
+    super()
+
+    this.state = {dataSource: ds.cloneWithRows([])}
+  }
+
+  _onPress() {
+    this.setState({dataSource: ds.cloneWithRows([]), error: undefined})
+
+    DocumentPicker.pickMultiple()
+    .then(files => this.setState({dataSource: ds.cloneWithRows(files)}),
+          error => this.setState({error}))
+  }
+
+  _renderRow({name, size, type, uri}) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View>
+        <Text>Name: {name}</Text>
+        <Text>Size: {size}</Text>
+        <Text>Type: {type}</Text>
+        <Text>Uri: {uri}</Text>
+      </View>
+    )
+  }
+
+  render() {
+    const {dataSource, error} = this.state
+
+    return (
+      <View>
+        <Button onPress={this._onPress.bind(this)} title="Select files"/>
+        <Text>{error && error.toString()}</Text>
+        <Text>{error && error.stack}</Text>
+        <ListView dataSource={dataSource} renderRow={this._renderRow}/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
