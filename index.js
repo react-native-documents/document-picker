0283de71ad3823125abb8ff66dbc916cf6ca914d
arelstone <arelstone@gmail.com>
2019-01-09 12:08:44 +0100

    default function param + no changing imutable opts

--- index.js
+++ index.js
@@ -93,16 +93,22 @@ export default class DocumentPicker {
    */
   static types = PlatformTypes[Platform.OS] || Types.mimeTypes
 
-  static pick(opts) {
-    opts = opts || {};
-    opts.multiple = false;
-    return pick(opts).then((results) => results[0]);
+  static pick(opts = {}) {
+    const options = {
+      ...opts,
+      multiple: false
+    }
+
+    return pick(options).then((results) => results[0]);
   }
 
-  static pickMultiple(opts) {
-    opts = opts || {};
-    opts.multiple = true;
-    return pick(opts);
+  static pickMultiple(opts = {}) {
+    const options = {
+      ...opts,
+      multiple: true
+    }
+
+    return pick(options);
   }
 
   static isCancel(err) {
