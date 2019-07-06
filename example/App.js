b488c1a150d4d3b10d3fded67b452cde25eb26bd
tkow <t.kusama0613@outlook.jp>
2019-07-07 00:06:50 +0900

    example change for catching error

--- example/App.js
+++ example/App.js
@@ -20,12 +20,23 @@ const App = () => {
           title={'popup test'}
           onPress={
             async ()=> {
-              const result = await DocumentPicker.pick(
-                {
-                  type: [DocumentPicker.types.images]
+              try {
+                const res = await DocumentPicker.pick({
+                  type: [DocumentPicker.types.images],
+                });
+                console.log(
+                  res.uri,
+                  res.type, // mime type
+                  res.name,
+                  res.size
+                );
+              } catch (err) {
+                if (DocumentPicker.isCancel(err)) {
+                  // User cancelled the picker, exit any dialogs or menus and move on
+                } else {
+                  throw err;
                 }
-              )
-              console.log(result)
+              }
             }
           }
         />
