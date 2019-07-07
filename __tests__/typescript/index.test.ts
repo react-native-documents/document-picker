282f289a73d98465a308201e8f3d7809fd527c83
tkow <t.kusama0613@outlook.jp>
2019-07-08 00:03:46 +0900

    add test case

--- __tests__/typescript/index.test.ts
+++ __tests__/typescript/index.test.ts
@@ -1,11 +1,65 @@
 import { DocumentPicker} from "react-native-document-picker";
 
-DocumentPicker.types.allFiles
-DocumentPicker.types.audio
-DocumentPicker.types.images
-DocumentPicker.types.plainText
-DocumentPicker.types.video
+// Option is correct about pick
 
 DocumentPicker.pick({
   type: [DocumentPicker.types.allFiles]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.audio]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.images]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.plainText]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.video]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.pdf]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.video,DocumentPicker.types.pdf, 'public.audio']
+})
+
+// Option is correct about pickMultiple
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.allFiles]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.allFiles]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.audio]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.images]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.plainText]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.video]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.pdf]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.video,DocumentPicker.types.pdf, 'public.audio']
 })
