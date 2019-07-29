a1f1821b6d5ee72f76aefff08e7725e9d41ededc
tkow <t.kusama0613@outlook.jp>
2019-07-29 14:24:58 +0900

    run android debug

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -29,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected String getJSMainModuleName() {
-      return "index";
+      return "example/index";
     }
   };
 
