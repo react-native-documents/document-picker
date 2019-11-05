9ebf035e783c288ad15dacda3180b97607473e7a
Rida Sarwar Bilgrami <rida_rocks12@yahoo.com>
2019-11-05 13:25:40 +0500

    enhance document support

--- __tests__/typescript/index.test.ts
+++ __tests__/typescript/index.test.ts
@@ -24,9 +24,32 @@ DocumentPicker.pick({
 DocumentPicker.pick({
   type: [DocumentPicker.types.pdf]
 })
+DocumentPicker.pick({
+  type: [DocumentPicker.types.doc]
+})
 
 DocumentPicker.pick({
-  type: [DocumentPicker.types.video,DocumentPicker.types.pdf, 'public.audio']
+  type: [DocumentPicker.types.docx]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.xls]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.xlsx]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.ppt]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.pptx]
+})
+
+DocumentPicker.pick({
+  type: [DocumentPicker.types.video,DocumentPicker.types.pdf, 'public.audio', DocumentPicker.types.doc, DocumentPicker.types.docx, DocumentPicker.types.ppt, DocumentPicker.types.pptx, DocumentPicker.types.xls, DocumentPicker.types.xlsx]
 })
 
 // Option is correct about pickMultiple
@@ -60,13 +83,37 @@ DocumentPicker.pickMultiple({
 })
 
 DocumentPicker.pickMultiple({
-  type: [DocumentPicker.types.video,DocumentPicker.types.pdf, 'public.audio']
+  type: [DocumentPicker.types.doc]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.docx]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.ppt]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.pptx]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.xls]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.xlsx]
+})
+
+DocumentPicker.pickMultiple({
+  type: [DocumentPicker.types.video,DocumentPicker.types.pdf, 'public.audio', DocumentPicker.types.ppt, DocumentPicker.types.pptx, DocumentPicker.types.doc, DocumentPicker.types.docx, DocumentPicker.types.xls, DocumentPicker.types.xlsx]
 })
 
 DocumentPicker.pick({type: 'image/jpg'});
 DocumentPicker.pick({type: 'public.png'});
 
-DocumentPicker.pick({type: ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf']});
+DocumentPicker.pick({type: ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf', 'application/doc', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' | 'application/vnd.ms-excel' | 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation']});
 DocumentPicker.pick({type: ['public.png', 'public.jpeg']});
 
 
