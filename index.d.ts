3240034f118e6face5db8b7a06e65b8c2423eca6
Rida Sarwar Bilgrami <rida_rocks12@yahoo.com>
2019-11-05 13:19:03 +0500

    multiple documents support

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 declare module 'react-native-document-picker' {
-  type UTI = 'public.png' | 'public.jpeg' | 'com.adobe.pdf';
-  type MimeType = 'image/jpg' | 'image/jpeg' | 'image/png' | 'application/pdf';
-  type Extension = '.jpeg' | '.jpg' | '.png' | '.txt' | '.pdf';
+  type UTI = 'public.png' | 'public.jpeg' | 'com.adobe.pdf' | 'com.adobe.doc' | 'com.adobe.docx'| 'com.adobe.xls' | 'com.adobe.xlsx';
+  type MimeType = 'image/jpg' | 'image/jpeg' | 'image/png' | 'application/pdf' | 'application/doc' | 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' | 'application/vnd.ms-excel' | 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' | 'application/vnd.ms-powerpoint' |'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+    ;
+  type Extension = '.jpeg' | '.jpg' | '.png' | '.txt' | '.pdf' | '.doc' | '.docx' | '.xls' | '.xlsx' | '.ppt' | '.pptx';
 
   type DocumentType = {
     android: MimeType | MimeType[]
@@ -17,6 +18,13 @@ declare module 'react-native-document-picker' {
       plainText: 'text/plain',
       pdf: 'application/pdf',
       video: 'video/*',
+      doc: 'application/doc',
+      docx: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      xls: 'application/vnd.ms-excel',
+      xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      ppt: 'application/vnd.ms-powerpoint',
+      pptx:
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation'
     },
     utis: {
       allFiles: 'public.content',
@@ -25,15 +33,27 @@ declare module 'react-native-document-picker' {
       plainText: 'public.plain-text',
       pdf: 'com.adobe.pdf',
       video: 'public.movie',
+      doc: 'com.adobe.doc',
+      docx: 'com.adobe.docx',
+      xls: 'com.adobe.xls',
+      xlsx: 'com.adobe.xlsx',
+      ppt: 'com.adobe.ppt',
+      pptx: 'com.adobe.pptx'
     },
     extensions: {
       allFiles: '*',
       audio:
-        '.3g2 .3gp .aac .adt .adts .aif .aifc .aiff .asf .au .m3u .m4a .m4b .mid .midi .mp2 .mp3 .mp4 .rmi .snd .wav .wax .wma',
+      '.3g2 .3gp .aac .adt .adts .aif .aifc .aiff .asf .au .m3u .m4a .m4b .mid .midi .mp2 .mp3 .mp4 .rmi .snd .wav .wax .wma',
       images: '.jpeg .jpg .png',
       plainText: '.txt',
       pdf: '.pdf',
       video: '.mp4',
+      doc: '.doc',
+      docx: '.docx',
+      xls: '.xls',
+      xlsx: '.xlsx',
+      ppt: '.ppt',
+      pptx: '.pptx'
     },
   };
   type PlatformTypes = {
@@ -59,6 +79,6 @@ declare module 'react-native-document-picker' {
     static pickMultiple<OS extends keyof PlatformTypes = Platform>(
       options: DocumentPickerOptions<OS>
     ): Promise<DocumentPickerResponse[]>;
-    static isCancel<IError extends {code?: string}>(err?: IError): boolean;
+    static isCancel<IError extends { code?: string }>(err?: IError): boolean;
   }
 }
