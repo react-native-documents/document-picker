d0f2d18295eeb6f472054e008b7c8b3ef27ba585
Matteo Pagani <matteo.pagani@microsoft.com>
2021-08-02 17:07:02 +0200

    Added windows example (not working for the moment)

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,31 +1,23 @@
-const path = require('path')
-const blacklist = require('metro-config/src/defaults/exclusionList')
-const escape = require('escape-string-regexp')
-const pak = require('../package.json')
-
-const root = path.resolve(__dirname, '..')
-
-const modules = Object.keys({
-  ...pak.peerDependencies,
-})
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require('path');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 module.exports = {
-  projectRoot: __dirname,
-  watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
-      modules.map((m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)),
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name)
-      return acc
-    }, {}),
+    blockList: exclusionList([
+      // This stops "react-native run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
+      /.*\.ProjectImports\.zip/,
+    ]),
   },
-
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -34,4 +26,4 @@ module.exports = {
       },
     }),
   },
-}
+};
