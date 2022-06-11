787f5b2de0d55ce4d09a0dc1c09055ee9e460506
Gurupad Mamadapur <gurupadmamadapur@gmail.com>
2022-06-11 14:11:22 +0530

    fix: intent type not set in case of multiple mime types filter (#557)

--- android/src/main/java/com/reactnativedocumentpicker/DocumentPickerModule.java
+++ android/src/main/java/com/reactnativedocumentpicker/DocumentPickerModule.java
@@ -131,6 +131,7 @@ public class DocumentPickerModule extends ReactContextBaseJavaModule {
           if (types.size() > 1) {
             String[] mimeTypes = readableArrayToStringArray(types);
             intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+            intent.setType(String.join("|",mimeTypes));
           } else if (types.size() == 1) {
             intent.setType(types.getString(0));
           }
