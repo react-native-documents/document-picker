544b1ebc4f671b201fcee2e8105dc14a7d397a47
930r91na <georgina.zeronca@udlap.mx>
2025-03-19 17:37:06 -0600

    fix: add missing error exports to document viewer test setup

--- packages/document-viewer/jest/setup.ts
+++ packages/document-viewer/jest/setup.ts
@@ -1,7 +1,10 @@
 type ExportedModuleType = typeof import('../src/index')
+import { errorCodes, isErrorWithCode } from '../src'
 
 const mockModule: ExportedModuleType = Object.freeze({
   viewDocument: jest.fn().mockResolvedValue(null),
+  errorCodes,
+  isErrorWithCode,
 })
 
 jest.mock('@react-native-documents/viewer', () => {
