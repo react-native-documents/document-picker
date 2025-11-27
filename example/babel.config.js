d47ab3e2a75bed5eb55b0d4a8efb83a0c0c9cc03
Vojtech Novak <vonovak@gmail.com>
2025-11-27 20:55:59 +0100

    fix: clean up gradle files (#890)
    * fix: clean up gradle files
    
    * Remove patch entry for 'react-native-documents-example'
    
    Removed patch entry for 'react-native-documents-example' from changeset.

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,29 @@
+const path = require('path')
+const root = path.resolve(__dirname, '..', 'packages')
+
+const pickerPath = path.join(
+  root,
+  'document-picker',
+  require('../packages/document-picker/package.json').exports['.'].source,
+)
+const viewerPath = path.join(
+  root,
+  'document-viewer',
+  require('../packages/document-viewer/package.json').exports['.'].source,
+)
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+        alias: {
+          '@react-native-documents/picker': pickerPath,
+          '@react-native-documents/viewer': viewerPath,
+        },
+      },
+    ],
+  ],
 }
